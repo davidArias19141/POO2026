fe98f44a3bee9ec91af191e0fee05ce40a97f240
davidArias19141 <david.arias70297@ucaldas.edu.co>
2026-09-15 11:20:02 -0500

    Dev 2, I complete the testthermomether, in the terminal, it's works well

--- TALLERES/TALLER 1/TestThermometer.java
+++ TALLERES/TALLER 1/TestThermometer.java
@@ -1,3 +1,23 @@
-public class TestThermometer{
+public class TestThermometer {
     
+    public static void main(String[] args) {
+
+    Thermometer Temperature = new Thermometer();
+
+        if(Temperature.setTemperature(-100)){
+            System.out.println("This temperature is into the correct range");
+        } else{ 
+            System.out.println("This temperature is out of the correct range");
+        }
+        if(Temperature.setTemperature(50)){
+            System.out.println("This temperature is into the correct range");
+        } else{ 
+            System.out.println("This temperature is out of the correct range");
+        }
+        if(Temperature.setTemperature(200)){
+            System.out.println("This temperature is into the correct range");
+        } else{ 
+            System.out.println("This temperature is out of the correct range");
+        }
+    }
 }
--- TALLERES/TALLER 1/Thermometer.java
+++ TALLERES/TALLER 1/Thermometer.java
@@ -1,3 +1,16 @@
 public class Thermometer {
+    private double Temperature = 0.0;
     
+    public double getTemperature(){
+        return Temperature;
+    }
+    public boolean setTemperature(double NewTemperature){
+        if(NewTemperature >= -50 && NewTemperature <= 100){
+            Temperature = NewTemperature;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

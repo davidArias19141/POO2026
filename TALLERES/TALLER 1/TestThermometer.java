c784c92acf723d454b448bb57b532c8366d61f2e
davidArias19141 <david.arias70297@ucaldas.edu.co>
2026-09-15 10:31:58 -0500

    Complit folder id send

--- TALLERES/TALLER 1/TestThermometer.java
+++ TALLERES/TALLER 1/TestThermometer.java
@@ -1,11 +1,3 @@
-public class 
-TestThermometer {
-
-    public static void main (String[] args){
-        Thermometer thermometer = new Thermometer();
-            System.out.println("--- Pruebas del termómetro digital ---");
-            
-            System.out.println("Temperatura Inicial: " + thermometer.Get_Current_Temperature() + "°C");
-            System.out.println("");
-        }
-    }
+public class TestThermometer{
+    
+}
